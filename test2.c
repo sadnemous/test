2cec879d4f3753d52bd891779b9fc02d2aeb0ad8
n074800 <soumen.das@united.com>
2020-10-28 16:56:58 -0500

    corrected test2.c

--- test2.c
+++ test2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#define TYPE_LEN 4
 int main()
 {
   int i = 0;
-  char type[4];
+  char type[TYPE_LEN+1];
   for (i=1; i<10099;)
   {
-    snprintf(type,4, "%4.4d", i);
+    snprintf(type,TYPE_LEN+1, "%4.4d", i);
     printf("%s\n", type);
     i = i + 50;
   }
